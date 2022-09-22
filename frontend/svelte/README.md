985b94b10980b339e012cfb2b29e0f97cd393997
sprechtl <sprechtl@htl-steyr.ac.at>
2022-09-23 00:45:24 +0200

    Login API request implemented

--- frontend/svelte/README.md
+++ frontend/svelte/README.md
@@ -7,3 +7,7 @@
 ## Run the docker container
 
 ``docker run --name svelte -dp 5173:5173 svelte``
+
+## Mastercommand for rebuild run etc.
+
+``docker build -t svelte .;docker stop svelte;docker rm svelte; docker run --name svelte -dp 5173:5173 svelte``
--- frontend/svelte/src/routes/login/+page.svelte
+++ frontend/svelte/src/routes/login/+page.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
 
+    let user: string;
+    let password: string;
+
+
+    async function handleSubmit() {
+        console.log(user, password)
+        const endpoint = "http://localhost:1337/api/auth/local";
+        const payload = {
+            identifier: user,
+            password: password
+        };
+
+        const login = await fetch(endpoint, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+
+        const response = await login.json();
+        console.log(response);
+
+    }
 </script>
 
 <head>
@@ -10,16 +35,16 @@
 </head>
 
 <main class="form-signin w-100 m-auto">
-    <form>
+
         <img class="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="Logo" width="72" height="57">
         <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
         <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" bind:value={user}>
             <label for="floatingInput">Email address</label>
         </div>
         <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" bind:value={password}>
             <label for="floatingPassword">Password</label>
         </div>
 
@@ -28,9 +53,9 @@
                 <input type="checkbox" value="remember-me"> Remember me
             </label>
         </div>
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
-    </form>
+        <button class="w-100 btn btn-lg btn-primary" on:click={handleSubmit}>Sign in</button>
+        <p class="mt-5 mb-3 text-muted">&copy;2022</p>
+
 </main>
 
 
