3786a803a69fced4f7a4c3859a932635cdc6383f
j-weissen <jweissen@htl-steyr.ac.at>
2022-09-26 12:32:27 +0200

    Schema changed, findMany implemented

--- backend/strapi/src/api/note/controllers/note.js
+++ backend/strapi/src/api/note/controllers/note.js
@@ -4,6 +4,38 @@
  * note controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const {createCoreController} = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::note.note');
+module.exports = createCoreController('api::note.note', ({strapi}) => ({
+  /**
+   * Gives all, to the user related, notes.
+   * @param ctx
+   * @returns {Promise<string>}
+   */
+  async find(ctx) {
+    const userId = ctx.state.user.id;
+
+    const entries = await strapi.entityService.findMany('api::note.note', {
+      populate: ['owners'],
+      filters: {
+        owners: {
+          id: userId
+        }
+      },
+      sort: 'lastViewed'
+    });
+    return JSON.stringify(entries);
+  },
+  /**
+   * Finds the note by id. Only returns the note when the user is assigned.
+   * @param ctx
+   * @returns {Promise<void>}
+   */
+  async findOne(ctx) {
+    console.log("findOne")
+  }
+}));
+
+/**
+ *
+ */
